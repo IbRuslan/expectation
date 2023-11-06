625e73fee0de33369cb144972baea740a3719c37
IbRuslan <ribragimov2003@gmail.com>
2023-11-07 02:29:05 +0500

    commit 4

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -2,9 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://test-backend-server.site/public/api',
+    baseUrl: 'https://test-backend-server.site/public/api',
     prepareHeaders: headers => {
       headers.append('Referer', 'false')
+      headers.append('Accept', '*/*')
       headers.append('Content-Type', 'application/json')
     },
   }),
